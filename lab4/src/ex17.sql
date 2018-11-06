41e598d473963b5163d9539407c4826c8de7e32a
Emil Terman <terman.emil@gmail.com>
2018-11-06 09:44:14 +0200

    lab4: example with subquerry

--- lab4/src/ex17.sql
+++ lab4/src/ex17.sql
@@ -4,3 +4,10 @@ JOIN studenti_reusita AS sr
     ON profesori.Id_Profesor = sr.Id_Profesor
 WHERE
     Id_Student = 100
+
+-- SELECT DISTINCT Nume_Profesor, Prenume_Profesor
+-- FROM profesori
+-- JOIN studenti_reusita AS sr
+--     ON profesori.Id_Profesor = sr.Id_Profesor
+-- WHERE
+--     Id_Student IN (SELECT Id_Student FROM studenti_reusita WHERE Id_Student = 100)
